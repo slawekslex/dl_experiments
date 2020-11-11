901f5746a3b8bad4c30a62dc7e1e59f3405d42b5
Slawek Biel <slawek.biel@gmail.com>
2020-11-11 14:27:12 +0000

    final submit

--- Hateful/experiment_runner.py
+++ Hateful/experiment_runner.py
@@ -9,23 +9,31 @@ def draw_hypers():
     hypers['head_ps'] = random.uniform(0.5,.80)
     hypers['stem_ps'] = random.uniform(0.2,.30)
     hypers['head_linear'] = random.randint(400, 600)
-    hypers['max_masked'] = random.randint(0, hypers['max_tgt_length'] //3)
+    hypers['max_masked'] = random.randint(0, hypers['max_tgt_length'] //2)
     hypers['mask_prob'] = random.uniform(0, .30)
     hypers['vis_mask_prob'] = random.uniform(0.1, .30)
     hypers['lr'] = random.uniform(0.0002, 0.002)
     hypers['lr_mult'] = random.uniform(hypers['lr']/3e-5, hypers['lr']/1.5e-5)
     hypers['train_epochs'] = 8#random.randint(6, 12)
-    hypers['stem_file'] = 'checkpoints/lm_stem_long.pth'
-    hypers['mixup_alpha'] = random.uniform(0.2, 0.6)
+    hypers['stem_file'] ='checkpoints/lm_stem30drop_p2.pth'
     return hypers
     
     
 
 
-for exp_id in range(150, 200):
+for exp_id in range(300, 330):
+    hypers = draw_hypers()   
+    print(hypers)
     print('============= exp', exp_id, '\n', flush=True)
-    hypers = draw_hypers()    
-    com = ['python', 'run_experiment.py', f'--experiment_id={exp_id}']
+ 
+    com = ['python', 'run_experiment.py', f'--experiment_id={exp_id}a']
     for name,value in hypers.items():
         com.append(f'--{name}={value}')
+    #com.append('--run_without_valid')
+    subprocess.run(com)
+    
+    com = ['python', 'run_experiment.py', f'--experiment_id={exp_id}b']
+    for name,value in hypers.items():
+        com.append(f'--{name}={value}')
+    com.append('--run_without_valid')
     subprocess.run(com)
